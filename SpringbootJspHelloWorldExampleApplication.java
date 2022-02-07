3b3db97ee0e276e933bdf6fe175d348fcbfc09d6
pragati2944 <pragati.agarwal@RSK-BSL.COM>
2022-02-07 18:46:11 +0530

    first commit

---
+++ SpringbootJspHelloWorldExampleApplication.java
@@ -0,0 +1,14 @@
+package net.javaguides.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@ComponentScan(basePackages={"com.net.javaguides.springboot.controller"})
+public class SpringbootJspHelloWorldExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJspHelloWorldExampleApplication.class, args);
+	}
+
+}
